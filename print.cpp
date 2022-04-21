fc422f2d1d5df66863b7b89c04c1cf3e5c1ce45e
Matthew Russo <70534986+MiscellaneousMongoose@users.noreply.github.com>
2022-04-20 20:44:49 -0400

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -1,6 +1,122 @@
 
+
 #include <iostream>
 using namespace std;
+
+void left_to_mid(int *point_left, int *point_mid, int n){
+    int start = 0;
+    int end = 0;
+    for(int i=0; i<n; i++){
+        if (*(point_left + i) != 0){
+            start = i;
+            break;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if (*(point_mid + i) != 0){
+            break;
+        }
+        end = i;
+    }
+    *(point_mid + end) = *(point_left + start);
+    *(point_left + start) = 0;
+}//end left to middle function
+
+void left_to_right(int *point_left, int *point_right, int n){
+    int start = 0;
+    int end = 0;
+    for(int i=0; i<n; i++){
+        if (*(point_left + i) != 0){
+            start = i;
+            break;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if (*(point_right + i) != 0){
+            break;
+        }
+        end = i;
+    }
+    *(point_right + end) = *(point_left + start);
+    *(point_left + start) = 0;
+}//end left to right
+
+void mid_to_left(int *point_mid, int *point_left, int n){
+    int start = 0;
+    int end = 0;
+    for(int i=0; i<n; i++){
+        if (*(point_mid + i) != 0){
+            start = i;
+            break;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if (*(point_left + i) != 0){
+            break;
+        }
+        end = i;
+    }
+    *(point_left + end) = *(point_mid + start);
+    *(point_mid + start) = 0;
+}//end middle to left
+
+void mid_to_right(int *point_mid, int *point_right, int n){
+    int start = 0;
+    int end = 0;
+    for(int i=0; i<n; i++){
+        if (*(point_mid + i) != 0){
+            start = i;
+            break;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if (*(point_right + i) != 0){
+            break;
+        }
+        end = i;
+    }
+    *(point_right + end) = *(point_mid + start);
+    *(point_mid + start) = 0;
+}//end middle to right
+
+void right_to_left(int *point_right, int *point_left, int n){
+    int start = 0;
+    int end = 0;
+    for(int i=0; i<n; i++){
+        if (*(point_right + i) != 0){
+            start = i;
+            break;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if (*(point_left + i) != 0){
+            break;
+        }
+        end = i;
+    }
+    *(point_left + end) = *(point_right + start);
+    *(point_right + start) = 0;
+}
+
+
+void right_to_mid(int *point_right, int *point_mid, int n){
+    int start = 0;
+    int end = 0;
+    for(int i=0; i<n; i++){
+        if (*(point_right + i) != 0){
+            start = i;
+            break;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if (*(point_mid + i) != 0){
+            break;
+        }
+        end = i;
+    }
+    *(point_mid + end) = *(point_right + start);
+    *(point_right + start) = 0;
+}
 
 
 void print(int *point_left, int *point_mid, int *point_right, int n){
@@ -55,13 +171,14 @@ void print(int *point_left, int *point_mid, int *point_right, int n){
     for(int i=0; i<((12 * n)+6); i++){
         cout<<"=";
     }
+    cout <<endl;
 }
 
 
 
 int main()
 {
-    int n;
+       int n;
     cout<<"enter an int: ";
     cin>>n;
     int *point_left = new int[n];
@@ -79,7 +196,40 @@ int main()
          *(point_right+i) = 0;
     }
     
+    print(point_left, point_mid, point_right, n);    
+    
+    left_to_mid(point_left, point_mid, n);          //L to  M
+    
     print(point_left, point_mid, point_right, n);
+    
+    left_to_right(point_left, point_right, n);        //L to R
+    
+    print(point_left, point_mid, point_right, n);   
+    
+    mid_to_right(point_mid, point_right, n);         //M to R
+    
+    print(point_left, point_mid, point_right, n);
+    
+    left_to_mid(point_left, point_mid, n);           //L to M
+    
+    print(point_left, point_mid, point_right, n);
+    
+    right_to_left(point_right, point_left, n);         //R to L
+    
+    print(point_left, point_mid, point_right, n);
+    
+    right_to_mid(point_right, point_mid, n);          //R to M
+    
+    print(point_left, point_mid, point_right, n);
+    
+    left_to_mid(point_left, point_mid, n);         //L to M
+    
+    print(point_left, point_mid, point_right, n);
+    
+    mid_to_left(point_mid, point_left, n);            //M to L
+    
+    print(point_left, point_mid, point_right, n);
+    
     
     
     delete[] point_left;
